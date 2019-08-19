e021441407a895da658fecd794d819ea1476534a
root <root@mail.com>
2019-08-19 11:35:16 +0800

    提交1

---
+++ git1/src/com/git1.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * @author Jackwon
+ * @date 2019/8/19-11:32
+ */
+public class git1 {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+    }
+}
